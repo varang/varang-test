348657f184e25baf93a310c35b0ed01b88a73f7f
halil.agin <halil.agin@gmail.com>
2016-08-06 15:36:37 +0100

    first commit for spring-test.

---
+++ backend/org.varangproject.spring/src/main/java/org/varangproject/App.java
@@ -0,0 +1,14 @@
+package org.varangproject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	System.out.println("selam!");
+        System.out.println( "Hello World!" );
+    }
+}
